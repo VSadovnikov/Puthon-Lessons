773cc0d66a89cdcc8a1438fff1f057b3698379b0
Vasiliy Sadovnikov <basilsad1989@gmail.com>
2019-03-02 15:44:58 +0200

    Create exe

---
+++ variables.py
@@ -0,0 +1,14 @@
+a = int(input("Введите 1 значение"))
+b = int(input("Введите 2 значение"))
+
+sum = a+b
+print("Сумма:", sum)
+
+minus = a-b
+print("Разница:", minus)
+
+mult = a*b
+print("Умножение:", mult)
+
+delenie = a/b
+print("Деление:", delenie)
